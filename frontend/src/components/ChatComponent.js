85e02c42388458b069839818c8220a3c95e41b3c
santushtisharma10 <sharmasantushti1012@gmail.com>
2022-02-05 22:24:31 +0530

    frontend design

--- frontend/src/components/ChatComponent.js
+++ frontend/src/components/ChatComponent.js
@@ -1,28 +1,43 @@
-import { Avatar,IconButton } from '@mui/material';
+import { Avatar, IconButton } from '@mui/material';
 import React from 'react';
 
-import {MoreVert,  SearchOutlined} from '@mui/icons-material';
+import { MoreVert, SearchOutlined, InsertEmoticon, Mic } from '@mui/icons-material';
 
 
 
 function ChatComponent() {
   return <div className='chat'>
-      <div className='chat-header'>
-        <Avatar />
-        <div className='chat-headerInfo'>
-          <h3>Contact or Group</h3>
-          <p style={{color: "gray"}}>Last seen at..</p>
-        </div>
-        <div className='chat-headerRight'>
-        <IconButton>
-                    <SearchOutlined />
-                </IconButton>
-                <IconButton>
-                    <MoreVert />
-                </IconButton>
-                
-        </div>
+    <div className='chat-header'>
+      <Avatar />
+      <div className='chat-headerInfo'>
+        <h3>Contact or Group</h3>
+        <p style={{ color: "gray" }}>Last seen at..</p>
       </div>
+      <div className='chat-headerRight'>
+        <IconButton>
+          <SearchOutlined />
+        </IconButton>
+        <IconButton>
+          <MoreVert />
+        </IconButton>
+
+      </div>
+    </div>
+    <div className='chat-body'>
+      <p className='chat-msg'>
+        <span className='chat-name'>Anonymous</span>
+        This is a message
+        <span className='chat-time'>{new Date().toUTCString()}</span>
+      </p>
+    </div>
+    <div className='chat-footer'>
+      <InsertEmoticon />
+      <form>
+        <input placeholder='Type a message' type="text"/>
+        <button>Submit</button>
+      </form>
+      <Mic />
+    </div>
   </div>;
 }
 
